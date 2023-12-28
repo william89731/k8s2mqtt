d9e47a7bb6db56d4e2d607502c073af76803f8e4
william89731 <willystile@gmail.com>
2023-12-29 00:16:19 +0100

    code refactor

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-require('dotenv').config();
-const { spawn } = require('child_process');
-
-//console.log(process.env)
-const mqtt = require('mqtt');
-const { exec } = require("child_process");
+require('dotenv').config()
+const { spawn } = require('node:child_process')
+const mqtt = require('mqtt')
 const host = process.env.HOST
 const port = process.env.PORT
 const clientId = `k8s2mqtt_${Math.random().toString(16).slice(3)}`
@@ -14,37 +11,43 @@ const client = mqtt.connect(connectUrl, {
   connectTimeout: 4000,
   username: process.env.USERNAME,
   password: process.env.PASSWORD,
-  reconnectPeriod: 1000,
-});
+  reconnectPeriod: 1000
+})
 
 client.on('connect', () => {
   console.log('Connected to mqtt broker')
-});
+})
 
-client.on("reconnect", function () {
-  console.log("Reconnection starting");
-});
+client.on('reconnect', function () {
+  console.log('Reconnection starting')
+})
 
-client.on("offline", function () {
-  console.log("Currently offline. Please check internet!");
-});
+client.on('offline', function () {
+  console.log('Currently offline. Please check internet!')
+})
 
-client.on("error", function (error) {
-  console.log("Error occurred: " + error);
-});
+client.on('error', function (error) {
+  console.log('Error occurred: ' + error)
+})
 
-const child = spawn('kubectl', ['get','event','-A','--field-selector','type!=Normal','-o=jsonpath="{.message}"','-w', '--watch-only=true'])
-const topic = 'k8s2mqtt/warning'
+const kube = textToExecute => new Promise(resolve => {
+  const command = spawn(textToExecute, { shell: true })
 
+  command.stdout.on('data', (data) => {
+    console.log(`${data}`)
+    const topic = 'k8s2mqtt/warning'
+    client.publish(topic, data, { qos: 0, retain: false }, (error) => {
+      if (error) {
+        console.error(error)
+      }
+    })
+  })
 
-child.stderr.on('data', (data) => {
-  console.error(`child stderr:\n${data}`);
-});
+  command.on('close', () => resolve(main()))
+})
 
-child.stdout.on('data', (data) => {
-  console.log(`\n${data}`);
-  client.publish(topic, `\n${data}`, { qos: 0, retain: false });
+const main = async () => {
+  await kube('kubectl get event -A --field-selector type!=Normal -o=jsonpath="namespace: {.involvedObject.namespace} \nname: {.involvedObject.name}  \nreason: {.reason}\n" -w --watch-only=true')
+}
 
-});
-    
-
+main()
